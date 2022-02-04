7614fc1433f5c958c2ef383da35d430942ea9519
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-03 19:17:03 -0600

    Setting up Dev environment

---
+++ images/server/public/client.js
@@ -0,0 +1,17 @@
+$(document).ready(onReady);
+
+function onReady(){
+    $('#calcContainer').on('click', '.buttons', addMath);
+    $('#clearBtn').on('click', clearMath)
+}
+
+function addMath() {
+    // if ($("#inputField").val() == null) {
+    //     $("#inputField").val($(this).text());
+    //     } else
+         $("#inputField").val($("#inputField").val() + $(this).text())
+}
+
+function clearMath() {
+    $("#inputField").val('')
+}
